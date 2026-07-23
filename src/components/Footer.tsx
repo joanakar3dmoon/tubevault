d0a8185463381850ef74d8fefca578aa6d15860c
joanakar3dmoon <joanlazaro83@gmail.com>
2026-07-24 01:34:46 +0200

    Add src/components/Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>TubeVault © 2026 — Creado por r3dm</p>
+      </div>
+    </footer>
+  )
+}
